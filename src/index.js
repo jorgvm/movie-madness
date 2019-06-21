6706727959bb6a46f7c48483a7874f189d1fa3d8
Jorg van Maaren <hello@coppercup.nl>
2019-06-21 17:24:52 +0200

    fix global styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { injectGlobal } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 //
 import App from "./components/App";
 import reset from "./styling/reset.js";
 import basis from "./styling/basis.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  ${basis}
+`;
 
-// Global styles
-injectGlobal`${reset}${basis}`;
+const Root = (
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>
+);
+
+ReactDOM.render(Root, document.getElementById("root"));
